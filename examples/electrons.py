13e25bb179a4cb8d04d7d858933b658fe7fa2727
Jan Berges <jan.berges@uni-bremen.de>
2018-05-09 15:32:31 +0200

    Show density of states in electron example

--- examples/electrons.py
+++ examples/electrons.py
@@ -22,14 +22,40 @@ eps, psi, order = elphmod.dispersion.dispersion(comm, H, q,
     vectors=True, order=True)
 
 if comm.rank == 0:
-    print("Plot dispersion along G-M-K-G..")
+    print("Diagonalize Hamiltonian on uniform mesh..")
 
-    for i in range(psi.shape[2]):
+nk = 120
+
+eps_full = elphmod.dispersion.dispersion_full(comm, H, nk)
+
+if comm.rank == 0:
+    print("Calculate DOS of metallic band..")
+
+ne = 300
+
+e = np.linspace(eps_full[:, :, 0].min(), eps_full[:, :, 0].max(), ne)
+
+DOS = elphmod.dos.hexDOS(eps_full[:, :, 0], comm)(e)
+
+if comm.rank == 0:
+    print("Plot dispersion and DOS..")
+
+    fig, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
+
+    ax1.set_ylabel('energy (eV)')
+    ax1.set_xlabel('wave vector')
+    ax2.set_xlabel('density of states (1/eV)')
+
+    ax1.set_xticks(x[GMKG])
+    ax1.set_xticklabels('GMKG')
+
+    for i in range(H.size):
         X, Y = elphmod.plot.compline(x, eps[:, i],
             0.05 * (psi[:, :, i] * psi[:, :, i].conj()).real)
 
-        for j in range(psi.shape[1]):
-            plt.fill(X, Y[j], color='RCB'[j])
+        for j in range(H.size):
+            ax1.fill(X, Y[j], color='RCB'[j])
 
-    plt.xticks(x[GMKG], list('GMKG'))
+    ax2.fill(DOS, e, color='C')
+
     plt.show()
