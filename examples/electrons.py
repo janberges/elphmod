4b803dacacf0b718e31625d03f6b24e092fb0ccb
Jan Berges <jan.berges@uni-bremen.de>
2018-05-09 14:49:49 +0200

    Show band characters in electron example

--- examples/electrons.py
+++ examples/electrons.py
@@ -18,11 +18,18 @@ if comm.rank == 0:
 
 q, x, GMKG = elphmod.bravais.GMKG(corner_indices=True)
 
-e, order = elphmod.dispersion.dispersion(comm, H, q, order=True)
+eps, psi, order = elphmod.dispersion.dispersion(comm, H, q,
+    vectors=True, order=True)
 
 if comm.rank == 0:
     print("Plot dispersion along G-M-K-G..")
 
+    for i in range(psi.shape[2]):
+        X, Y = elphmod.plot.compline(x, eps[:, i],
+            0.05 * (psi[:, :, i] * psi[:, :, i].conj()).real)
+
+        for j in range(psi.shape[1]):
+            plt.fill(X, Y[j], color='RCB'[j])
+
     plt.xticks(x[GMKG], list('GMKG'))
-    plt.plot(x, e)
     plt.show()
