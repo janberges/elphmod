4acfa4a69ae9debc91ff8a28103ad8c7fb8dd574
Jan Berges <jan.berges@uni-bremen.de>
2018-05-15 17:24:31 +0200

    Add function to read el.-ph. coupling from EPW output

--- elphmod/coupling.py
+++ elphmod/coupling.py
@@ -54,6 +54,81 @@ def coupling(comm, filename, nQ, nb, nk, bands,
 
     return elph[:, :, 0, 0, :, :] if bands == 1 and squeeze else elph
 
+def read_EPW_output(comm, epw_out, q, nq, nb, nk, eps=1e-4):
+    """Read electron-phonon coupling from EPW output file."""
+
+    elph = np.empty((len(q), nb, nk, nk), dtype=complex)
+
+    if comm.rank == 0:
+        q_set = set(q)
+
+        elph[:] = np.nan
+
+        iq = None
+
+        with open(epw_out) as data:
+            for line in data:
+                if line.startswith('     iq = '):
+                    if not q_set:
+                        break
+
+                    iq = None
+
+                    columns = line.split()
+
+                    q1f = float(columns[-3]) * nq
+                    q2f = float(columns[-2]) * nq
+
+                    q1 = int(round(q1f))
+                    q2 = int(round(q2f))
+
+                    if abs(q1f - q1) > eps or abs(q2f - q2) > eps: # q in mesh?
+                        continue
+
+                    q1 %= nq
+                    q2 %= nq
+
+                    if not (q1, q2) in q_set: # q among chosen irred. points?
+                        continue
+
+                    iq = q.index((q1, q2))
+                    q_set.remove((q1, q2))
+
+                    print('q = (%d, %d)' % (q1, q2))
+
+                if iq is not None and line.startswith('     ik = '):
+                    columns = line.split()
+
+                    k1f = float(columns[-3]) * nk
+                    k2f = float(columns[-2]) * nk
+
+                    k1 = int(round(k1f))
+                    k2 = int(round(k2f))
+
+                    if abs(k1f - k1) > eps or abs(k2f - k2) > eps: # k in mesh?
+                        continue
+
+                    k1 %= nk
+                    k2 %= nk
+
+                    next(data)
+                    next(data)
+
+                    for nu in range(nb):
+                        columns = next(data).split()
+
+                        elph[iq, nu, k1, k2] = complex(
+                            float(columns[-2]), float(columns[-1]))
+
+        if np.isnan(elph).any():
+            print("Warning: EPW output incomplete!")
+
+        elph *= 1e-3 ** 1.5 # meV^(3/2) to eV^(3/2)
+
+    comm.Bcast(elph)
+
+    return elph
+
 def read(filename, nq, bands):
     """Read and complete Fermi-surface averaged electron-phonon coupling."""
 
