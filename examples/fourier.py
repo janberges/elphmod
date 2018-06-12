dfc6d13d5cf26e079e555a4724691d7d04208579
Jan Berges <jan.berges@uni-bremen.de>
2018-06-12 21:08:36 +0200

    Calculate number of non-equivalent n-th neighbors

---
+++ examples/fourier.py
@@ -0,0 +1,20 @@
+#/usr/bin/env python
+
+import elphmod
+
+N = 72
+
+points = dict()
+
+for n, m in elphmod.bravais.irreducibles(N, angle=60):
+    d = n * n + n * m + m * m
+
+    if d in points:
+        points[d].append((n, m))
+    else:
+        points[d] = [(n, m)]
+
+print('%4s  %4s  %s' % ('no.', 'r^2', 'irred. points'))
+
+for number, (key, value) in enumerate(sorted(points.items())):
+    print('%4d  %4d  %s' % (number, key, ', '.join(map(str, value))))
