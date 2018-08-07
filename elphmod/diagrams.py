ef2a79c89900843aa339dc1ca389e62d208b8717
Jan Berges <jan.berges@uni-bremen.de>
2018-08-07 16:29:26 +0200

    Add function to calculate RPA polarization

--- elphmod/diagrams.py
+++ elphmod/diagrams.py
@@ -46,12 +46,60 @@ def susceptibility(e, T=1.0, eta=1e-10):
 
     return calculate_susceptibility
 
+def polarization(e, c, T=1.0, i0=1e-10j):
+    """Calculate RPA polarization
+
+        Pi(q, a, b) = 2/N sum[k] <k+q|a><a|k><k|b><b|k+q>
+            [f(k+q) - f(k)] / [e(k+q) - e(k) + i0].
+
+    The resolution in q is limited by the resolution in k."""
+
+    nk, nk = e.shape
+    nk, nk, nb = c.shape # c[k1, k2, a] = <a|k>
+
+    kT = kB * T
+    x = e / kT
+
+    f = occupations.fermi_dirac(x)
+
+    e = np.tile(e, (2, 2))
+    f = np.tile(f, (2, 2))
+    c = np.tile(c, (2, 2, 1))
+
+    scale = nk / (2 * np.pi)
+    prefactor = 2.0 / nk ** 2
+
+    def calculate_polarization(q1=0, q2=0):
+        q1 = int(round(q1 * scale)) % nk
+        q2 = int(round(q2 * scale)) % nk
+
+        df = f[q1:q1 + nk, q2:q2 + nk] - f[:nk, :nk]
+        de = e[q1:q1 + nk, q2:q2 + nk] - e[:nk, :nk]
+
+        cc = c[:nk, :nk] * c[q1:q1 + nk, q2:q2 + nk].conj()
+
+        Pi = np.empty((nb, nb), dtype=complex)
+
+        for a in range(nb):
+            cca = cc[:, :, a]
+
+            for b in range(nb):
+                ccb = cc[:, :, b].conj()
+
+                Pi[a, b] = np.sum(cca * ccb * df / (de + i0))
+
+        return prefactor * Pi
+
+    calculate_polarization.size = nb
+
+    return calculate_polarization
+
 def phonon_self_energy(q, e, g2, T=100.0, i0=1e-10j,
         occupations=occupations.fermi_dirac):
     """Calculate phonon self-energy
 
         Pi(q, nu) = 2/N sum[k] |g(q, nu, k)|^2
-            [f(k+q) - f(k)] / [e(k+q) - e(k) + i eta]."""
+            [f(k+q) - f(k)] / [e(k+q) - e(k) + i0]."""
 
     nk, nk = e.shape
     nQ, nb, nk, nk = g2.shape
