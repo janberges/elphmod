3bbf481af71004eb2f687bfe54c1a27669aafde9
Jan Berges <jan.berges@uni-bremen.de>
2018-10-11 10:00:05 +0200

    Tidy up shared-memory routine and add usage example

--- elphmod/MPI.py
+++ elphmod/MPI.py
@@ -33,7 +33,37 @@ def distribute(size, bounds=False):
     return sizes
 
 def shared_array(shape, dtype):
-    "Create array whose memory is shared among all processes."
+    "Create array whose memory is shared among all processes on same node.
+
+    Example:
+
+    # Set up huge array:
+    node, images, array = shared_array(2 ** 30, dtype=np.uint8)
+
+    # Write data on one node:
+    if comm.rank == 0:
+        array[:] = 0
+
+    # Broadcast data to other nodes:
+    if node.rank == 0:
+        images.Bcast(array)
+                               ______ ______
+                    Figure:   |_0,_0_|_4,_0_|
+                              |_1,_1_|_5,_1_|
+    comm.rank and node.rank   |_2,_2_|_6,_2_|
+    on machine with 2 nodes   |_3,_3_|_7,_3_|
+    with 4 processors each.    node 1 node 2
+    """
+
+    # From article from Intel Developer Zone:
+    # 'An Introduction to MPI-3 Shared Memory Programming'
+
+    node = comm.Split_type(MPI.COMM_TYPE_SHARED, key=comm.rank) # same node
+
+    # From Gilles reply to StackOverflow question:
+    # 'get Nodes with MPI program in C'
+
+    images = comm.Split(node.rank, key=comm.rank) # same node.rank
 
     # Shared memory allocation following Lisandro Dalcin on Google Groups:
     # 'Shared memory for data structures and mpi4py.MPI.Win.Allocate_shared'
@@ -41,16 +71,6 @@ def shared_array(shape, dtype):
     size = np.prod(shape)
     dtype = np.dtype(dtype)
     itemsize = dtype.itemsize
-
-    # From article from Intel Developer Zone
-    # 'An Introduction to MPI-3 Shared Memory Programming':
-
-    node = comm.Split_type(MPI.COMM_TYPE_SHARED, key=comm.rank) # same node
-
-    # From Gilles reply to StackOverflow question
-    # 'get Nodes with MPI program in C':
-
-    images = comm.Split(node.rank, key=comm.rank) # same node.rank
 
     if node.rank == 0:
         bytes = size * itemsize
