5a4b5bac7080852436d53a7a3217e03fcc8251d3
Jan Berges <jan.berges@uni-bremen.de>
2018-11-30 16:58:05 +0100

    Add Wannier interpolation to new example

--- examples/phrenorm/1_electrons/run.sh
+++ examples/phrenorm/1_electrons/run.sh
@@ -8,4 +8,9 @@ mpirun -np 2 pw.x -nk 2 < Be.scf
 mpirun -np 2 pw.x -nk 2 < Be.bands
 mpirun -np 2 bands.x -nk 2 < bands.in
 
-echo "plot 'bands.dat.gnu' with lines" | gnuplot -p
+mpirun -np 2 pw.x -nk 2 < Be.nscf
+wannier90.x -pp Be
+mpirun -np 2 pw2wannier90.x < Be.pw2w90
+wannier90.x Be
+
+echo "p 'bands.dat.gnu' u (\$1*2*pi/2.07):2, 'Be_band.dat' w l" | gnuplot -p
