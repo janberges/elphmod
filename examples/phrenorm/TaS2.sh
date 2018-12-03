cd1fab888921504c81301a085f031b0116edba78
Jan Berges <jan.berges@uni-bremen.de>
2018-12-03 16:37:00 +0100

    Add second phonon-renormalization example using TaS2

---
+++ examples/phrenorm/TaS2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+for PP in S.pbe-hgh.UPF Ta.pbe-hgh.UPF
+do
+    test -e $PP || wget https://www.quantum-espresso.org/upf_files/$PP
+done
+
+mpirun -np 4 pw.x -nk 2 < TaS2.scf
+mpirun -np 4 pw.x -nk 2 < TaS2.bands
+mpirun -np 4 bands.x -nk 2 < TaS2.pltbnd
+
+mpirun -np 4 pw.x -nk 2 < TaS2.nscf
+wannier90.x -pp TaS2
+mpirun -np 4 pw2wannier90.x < TaS2.pw2w90
+wannier90.x TaS2
+
+echo "p 'bands.dat.gnu' u (\$1*2*pi/3.39):2, 'TaS2_band.dat' w l" | gnuplot -p
+
+mpirun -np 4 ph.x -nk 2 < TaS2.dfpt
