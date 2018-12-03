4b242d53d9fe9845cd9892058fd8897dfac08297
Jan Berges <jan.berges@uni-bremen.de>
2018-12-03 16:46:59 +0100

    Rename 'bands.x' input file of beryllium example

--- examples/phrenorm/Be.sh
+++ examples/phrenorm/Be.sh
@@ -6,7 +6,7 @@ test -e $PP || wget https://www.quantum-espresso.org/upf_files/$PP
 
 mpirun -np 2 pw.x -nk 2 < Be.scf
 mpirun -np 2 pw.x -nk 2 < Be.bands
-mpirun -np 2 bands.x -nk 2 < bands.in
+mpirun -np 2 bands.x -nk 2 < Be.pltbnd
 
 mpirun -np 2 pw.x -nk 2 < Be.nscf
 wannier90.x -pp Be
