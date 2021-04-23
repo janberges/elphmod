b7623fb0f6ee9b66ac4959f120e31e282b9bba3f
Jan Berges <jan.berges@uni-bremen.de>
2021-04-23 19:44:04 +0200

    Tidy up logo script and ignore output file

--- logo/logo.sh
+++ logo/logo.sh
@@ -2,16 +2,15 @@
 
 w=12
 h=9
-px=640
 in=2.54
 
 pdflatex --interaction=batchmode logo
 
-convert -density `perl -e "print $px / ($w / $in)"` logo.pdf \
+convert -density `perl -e "print 640 / ($w / $in)"` logo.pdf \
     -flatten PNG8:logo.png
 
-convert -density `perl -e "print $px / ($w / $in)"` logo.pdf \
+convert -density `perl -e "print 640 / ($w / $in)"` logo.pdf \
     -background white -gravity center -extent 640x640 PNG8:logo_square.png
 
-convert -density `perl -e "print $px / ($h / $in)"` logo.pdf \
+convert -density `perl -e "print 640 / ($h / $in)"` logo.pdf \
     -background white -gravity center -extent 1280x640 PNG8:logo_banner.png
