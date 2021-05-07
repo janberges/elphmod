3d5f4ceee9c6fc1674c171da4b9ab88c44ace03f
Jan Berges <jan.berges@uni-bremen.de>
2021-05-07 17:28:06 +0200

    Add setup.cfg and pyproject.toml build files

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ with open('README.md', 'r', encoding='utf-8') as README:
 
 setuptools.setup(
     name                          = 'elphmod',
-    version                       = '2021.1',
+    version                       = '0.1',
     author                        = 'elphmod Developers',
     author_email                  = '',
     description                   = 'Modules to handle electron-phonon models',
     long_description              = long_description,
     long_description_content_type = 'text/markdown',
-    url                           = 'https://bitbucket.org/berges/elphmod',
+    url                           = 'https://github.com/janberges/elphmod',
     packages                      = setuptools.find_packages(),
     python_requires               = '>=2.7',
     install_requires              = ['numpy'],
@@ -24,12 +24,5 @@ setuptools.setup(
         'Programming Language :: Python',
         'License :: OSI Approved :: GNU General Public License (GPL)',
         'Operating System :: POSIX :: Linux',
-        ],
-    scripts = [
-        'bin/kpoints',
-        'bin/minimum',
-        'bin/ph2epw',
-        'bin/qe_mod_6.3',
-        'bin/qe_mod_6.7',
         ],
     )
