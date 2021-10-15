6ab0739c560fccbabf24fc153c5670fb609b9775
Jan Berges <jan.berges@uni-bremen.de>
2021-10-15 14:00:59 +0200

    Optimize displacements directions on supercell

---
+++ examples/skiing.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2021 elphmod Developers
+# This program is free software under the terms of the GNU GPLv3 or later.
+
+import elphmod
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.optimize
+
+ph = elphmod.ph.Model('data/NbSe2_DFPT.ifc', divide_mass=False)
+ph = ph.supercell(3, 3)
+C = ph.D()
+
+def E2(u):
+    u /= np.linalg.norm(u)
+    return np.einsum('x,xy,y', u, C, u).real
+
+u = 1 - 2 * np.random.rand(ph.r.size)
+u = scipy.optimize.minimize(E2, u).x
+u /= u.max()
+
+if elphmod.MPI.comm.rank == 0:
+    Nb = np.array([X == 'Nb' for X in ph.atom_order])
+
+    ax = plt.axes(projection='3d')
+    ax.set_box_aspect(np.ptp(ph.r, axis=0))
+    ax.set_axis_off()
+
+    ax.scatter(*ph.r[Nb].T, s=100.0, c='k')
+    ax.scatter(*ph.r[~Nb].T, s=50.0, c='y')
+
+    ax.quiver(*ph.r.T, *u.reshape(ph.r.shape).T)
+
+    plt.show()
