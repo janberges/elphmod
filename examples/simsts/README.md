a5a21ae36907e86f768eec35a84788356f9483d1
Jan Berges <jan.berges@uni-bremen.de>
2021-12-03 23:32:32 +0100

    Simulate STM image using Wannier functions

--- examples/simsts/README.md
+++ examples/simsts/README.md
@@ -3,13 +3,14 @@
 This example shows how to
 
 * work with Wannier functions in the position representation from Wannier90,
-* simulate scanning-tunnelling spectra (STS) using these Wannier functions.
+* simulate scanning-tunnelling microscopy (STM) and spectroscopy (STS) data.
 
-The STS is calculated just like the density of states, except that each state is
-weighted with the square modulus of its overlap with an s orbital located at a
-chosen position above the sample. To correctly calculate this overlap, the same
-gauge must be applied to the Wannier functions used to represent the Hamiltonian
-(*seedname_hr.dat*) and those written to file (*seedname_0000X.xsf*). Hence, the
-following line in *plot.F90* of Wannier90 must be commented out:
+The Wannier functions used to represent the Hamiltonian (*seedname_hr.dat*) and
+those written to file (*seedname_0000X.xsf*) must be identical (including sign).
+Hence, the following line in *plot.F90* of Wannier90 must be commented out:
 
     wann_func(:, :, :, loop_w) = wann_func(:, :, :, loop_w)/wmod
+
+The scanning-tunnelling spectrum is calculated just like the density of states,
+except that each state is weighted with the square modulus of its overlap with
+an s orbital located at a chosen position above the sample.
--- examples/simsts/simsts.py
+++ examples/simsts/simsts.py
@@ -13,6 +13,7 @@ info = elphmod.MPI.info
 nk = 72
 
 tip = np.array([0.0, 0.0, 3.0]) # tip position
+V = 0.1 * elphmod.misc.Ry # sample bias
 
 info('Set up and diagonalize Wannier Hamiltonian..')
 
@@ -40,25 +41,51 @@ w, dw = np.linspace(e.min(), e.max(), 300, retstep=True)
 
 DOS = sum(elphmod.dos.hexDOS(e[:, :, n])(w) for n in range(el.size))
 
-info('Calculate overlap of tip and Wannier orbitals..')
+info('Determine indices of unit-cell corners and vertical tip position..')
+
+def indices(point):
+    return np.unravel_index(np.argmin(np.linalg.norm(el.r - point,
+        axis=3)), el.r.shape[:3])
+
+x0, y0, z0 = indices(0.0)
+x1, y1, z1 = indices(a.sum(axis=0))
+xt, yt, zt = indices(tip)
+
+nx = x1 - x0
+ny = y1 - y0
+
+info('Shift Wannier functions and calculate overlap with tip..')
 
 if comm.rank == 0:
     cells = range(-3, 4)
     R = np.array([(n1, n2, 0) for n1 in cells for n2 in cells])
 
+    W = np.empty((len(R),) + el.W.shape)
     overlap = np.empty((len(R), el.size))
+
     norm = np.sqrt(np.pi * elphmod.misc.a0 ** 3)
+    d = np.linalg.norm(tip - el.r, axis=3)
+    s = np.exp(-d / elphmod.misc.a0) / norm
 
     for iR in range(len(R)):
-        shift = np.dot(R[iR], a)
-        d = np.linalg.norm(tip - shift - el.r, axis=3)
-        s = np.exp(-d / elphmod.misc.a0) / norm
+        dx = R[iR, 0] * nx
+        dy = R[iR, 1] * ny
 
         for n in range(el.size):
-            overlap[iR, n] = np.sum(s * el.W[n]) * el.dV
+            W[iR, n] = el.W[n]
+            W[iR, n] = np.roll(W[iR, n], shift=dx, axis=0)
+            W[iR, n] = np.roll(W[iR, n], shift=dy, axis=1)
 
-info('Calculate weights of electronic eigenstates..')
+            if dx > 0: W[iR, n, :dx, :] = 0.0
+            if dx < 0: W[iR, n, dx:, :] = 0.0
+            if dy > 0: W[iR, n, :, :dy] = 0.0
+            if dy < 0: W[iR, n, :, dy:] = 0.0
 
+            overlap[iR, n] = np.sum(s * W[iR, n]) * el.dV
+
+info('Calculate scanning-tunneling image and weight electronic eigenstates..')
+
+STM = np.zeros((nx, ny))
 weight = np.empty(e.shape)
 
 if comm.rank == 0:
@@ -66,13 +93,41 @@ if comm.rank == 0:
 
     for k1 in range(nk):
         for k2 in range(nk):
+            for n in range(el.size):
+                if 0.0 < e[k1, k2, n] < V:
+                    tmp = 0.0
+                    for iR in range(len(R)):
+                        tmp += np.einsum('nxy,n', W[iR, :, x0:x1, y0:y1, zt],
+                            U[k1, k2, :, n]) * np.exp(1j
+                            * (k1 * R[iR, 0] + k2 * R[iR, 1]) * scale)
+                    STM += abs(tmp) ** 2
+
             tmp = 0.0
             for iR in range(len(R)):
                 tmp += np.dot(overlap[iR], U[k1, k2]) * np.exp(1j
                     * (k1 * R[iR, 0] + k2 * R[iR, 1]) * scale)
+
             weight[k1, k2] = (abs(tmp) / nk) ** 2
 
+comm.Bcast(STM)
 comm.Bcast(weight)
+
+info('Plot scanning-tunnelling image..')
+
+AFMhot = elphmod.plot.colormap( # Gnuplot
+    (0.00, elphmod.plot.Color(0, 0, 0)),
+    (0.25, elphmod.plot.Color(128, 0, 0)),
+    (0.50, elphmod.plot.Color(255, 128, 0)),
+    (0.75, elphmod.plot.Color(255, 255, 128)),
+    (1.00, elphmod.plot.Color(255, 255, 255)),
+    )
+
+plot = elphmod.plot.plot(STM, angle=120)
+
+image = elphmod.plot.color(plot, AFMhot)
+
+if comm.rank == 0:
+    elphmod.plot.save('simstm.png', image)
 
 info('Calculate scanning-tunnelling spectrum..')
 
