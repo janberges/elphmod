Fix sign in Marzari-Vanderbilt smearing

elphmod/occupations.py
@@ -47,9 +47,9 @@ gauss.delta = gauss_delta
 def marzari_vanderbilt(x):
     """Calculate Marzari-Vanderbilt (cold smearing) step function."""
 
-    y = -x - 1 / np.sqrt(2)
+    y = x + 1 / np.sqrt(2)
 
-    return (math.erf(y) + 1) / 2 + np.exp(-y * y) / np.sqrt(2 * np.pi)
+    return (math.erf(-y) + 1) / 2 + np.exp(-y * y) / np.sqrt(2 * np.pi)
 
 if not_doc:
     marzari_vanderbilt = np.vectorize(marzari_vanderbilt)
@@ -57,9 +57,9 @@ if not_doc:
 def marzari_vanderbilt_delta(x):
     """Calculate negative derivative of Marzari-Vanderbilt step function."""
 
-    y = x - 1 / np.sqrt(2)
+    y = x + 1 / np.sqrt(2)
 
-    return np.exp(-y * y) * (2 - np.sqrt(2) * x) / np.sqrt(np.pi)
+    return (np.sqrt(2) * x + 2) * np.exp(-y * y) / np.sqrt(np.pi)
 
 marzari_vanderbilt.delta = marzari_vanderbilt_delta
 

examples/occupations.py
@@ -7,7 +7,7 @@ import numpy as np
 if MPI.comm.rank:
     raise SystemExit
 
-x = np.linspace(-5, 5, 1000)
+x, dx = np.linspace(-5, 5, 1000, retstep=True)
 
 style = dict(color='lightgray', linestyle='dashed')
 
@@ -39,6 +39,13 @@ plt.plot(x, occupations.gauss.delta(x), label='Gauss')
 plt.plot(x, occupations.marzari_vanderbilt.delta(x), label='Marzari-Vanderbilt')
 plt.plot(x, occupations.methfessel_paxton.delta(x), label='Methfessel-Paxton')
 plt.plot(x, occupations.lorentz.delta(x), label='Lorentz')
+
+X = (x[1:] + x[:-1]) / 2
+plt.plot(X, -np.diff(occupations.fermi_dirac(x)) / dx, 'k--')
+plt.plot(X, -np.diff(occupations.gauss(x)) / dx, 'k--')
+plt.plot(X, -np.diff(occupations.marzari_vanderbilt(x)) / dx, 'k--')
+plt.plot(X, -np.diff(occupations.methfessel_paxton(x)) / dx, 'k--')
+plt.plot(X, -np.diff(occupations.lorentz(x)) / dx, 'k--')
 
 plt.xlabel = r'$x$'
 plt.ylabel = r'$\delta(x)$'