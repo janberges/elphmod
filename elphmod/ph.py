Fix sign of primitive vectors for negative BZ volumes

elphmod/ph.py
@@ -139,11 +139,13 @@ class Model(object):
         N2 = np.array(N2)
         N3 = np.array(N3)
 
-        N = abs(np.dot(N1, np.cross(N2, N3)))
+        N = np.dot(N1, np.cross(N2, N3))
 
-        B1 = np.cross(N2, N3)
-        B2 = np.cross(N3, N1)
-        B3 = np.cross(N1, N2)
+        B1 = np.sign(N) * np.cross(N2, N3)
+        B2 = np.sign(N) * np.cross(N3, N1)
+        B3 = np.sign(N) * np.cross(N1, N2)
+
+        N = abs(N)
 
         ph = Model()
         ph.M = np.tile(self.M, N)