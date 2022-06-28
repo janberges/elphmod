8af5fde2f0bfefc7454f92319e9384f151853e94
Jan Berges <jan.berges@uni-bremen.de>
2022-06-28 19:15:00 +0200

    Plot double Fermi-Dirac before Lorentz smearing

--- examples/occupations.py
+++ examples/occupations.py
@@ -24,6 +24,8 @@ plt.plot(x, occupations.fermi_dirac(x), label='Fermi-Dirac')
 plt.plot(x, occupations.gauss(x), label='Gauss')
 plt.plot(x, occupations.marzari_vanderbilt(x), label='Marzari-Vanderbilt')
 plt.plot(x, occupations.methfessel_paxton(x), label='Methfessel-Paxton')
+plt.plot(x, occupations.double_fermi_dirac(x, mu1, mu2),
+    label='Double Fermi-Dirac')
 plt.plot(x, occupations.lorentz(x), label='Lorentz')
 
 plt.xlabel(r'$x$')
@@ -40,16 +42,17 @@ plt.plot(x, occupations.fermi_dirac.delta(x), label='Fermi-Dirac')
 plt.plot(x, occupations.gauss.delta(x), label='Gauss')
 plt.plot(x, occupations.marzari_vanderbilt.delta(x), label='Marzari-Vanderbilt')
 plt.plot(x, occupations.methfessel_paxton.delta(x), label='Methfessel-Paxton')
+plt.plot(x, occupations.double_fermi_dirac.delta(x, mu1, mu2),
+    label='Double Fermi-Dirac')
 plt.plot(x, occupations.lorentz.delta(x), label='Lorentz')
-plt.plot(x, occupations.double_fermi_dirac.delta(x, mu1,mu2), label='Double Fermi-Dirac')
 
 X = (x[1:] + x[:-1]) / 2
 plt.plot(X, -np.diff(occupations.fermi_dirac(x)) / dx, 'k--')
 plt.plot(X, -np.diff(occupations.gauss(x)) / dx, 'k--')
 plt.plot(X, -np.diff(occupations.marzari_vanderbilt(x)) / dx, 'k--')
 plt.plot(X, -np.diff(occupations.methfessel_paxton(x)) / dx, 'k--')
-plt.plot(X, -np.diff(occupations.lorentz(x)) / dx, 'k--')
 plt.plot(X, -np.diff(occupations.double_fermi_dirac(x, mu1, mu2)) / dx, 'k--')
+plt.plot(X, -np.diff(occupations.lorentz(x)) / dx, 'k--')
 
 plt.xlabel(r'$x$')
 plt.ylabel(r'$\delta(x)$')
@@ -67,16 +70,18 @@ plt.plot(x, occupations.marzari_vanderbilt.delta_prime(x),
     label='Marzari-Vanderbilt')
 plt.plot(x, occupations.methfessel_paxton.delta_prime(x),
     label='Methfessel-Paxton')
+plt.plot(x, occupations.double_fermi_dirac.delta_prime(x, mu1, mu2),
+    label='Double Fermi-Dirac')
 plt.plot(x, occupations.lorentz.delta_prime(x), label='Lorentz')
-plt.plot(x, occupations.double_fermi_dirac.delta_prime(x, mu1, mu2), label='Double Fermi-Dirac')
 
 X = (X[1:] + X[:-1]) / 2
 plt.plot(X, -np.diff(occupations.fermi_dirac(x), 2) / dx ** 2, 'k--')
 plt.plot(X, -np.diff(occupations.gauss(x), 2) / dx ** 2, 'k--')
 plt.plot(X, -np.diff(occupations.marzari_vanderbilt(x), 2) / dx ** 2, 'k--')
 plt.plot(X, -np.diff(occupations.methfessel_paxton(x), 2) / dx ** 2, 'k--')
+plt.plot(X, -np.diff(occupations.double_fermi_dirac(x, mu1, mu2), 2) / dx ** 2,
+    'k--')
 plt.plot(X, -np.diff(occupations.lorentz(x), 2) / dx ** 2, 'k--')
-plt.plot(X, -np.diff(occupations.double_fermi_dirac(x, mu1, mu2), 2) / dx ** 2, 'k--')
 
 plt.xlabel(r'$x$')
 plt.ylabel(r"$\delta'(x)$")
@@ -93,13 +98,15 @@ plt.plot(x, occupations.gauss.entropy(x), label='Gauss')
 plt.plot(x, occupations.marzari_vanderbilt.entropy(x),
     label='Marzari-Vanderbilt')
 plt.plot(x, occupations.methfessel_paxton.entropy(x), label='Methfessel-Paxton')
-plt.plot(x, occupations.double_fermi_dirac.entropy(x, mu1, mu2), label='Double Fermi-Dirac')
+plt.plot(x, occupations.double_fermi_dirac.entropy(x, mu1, mu2),
+    label='Double Fermi-Dirac')
 
 plt.plot(x, -dx * np.cumsum(x * occupations.fermi_dirac.delta(x)), 'k--')
 plt.plot(x, -dx * np.cumsum(x * occupations.gauss.delta(x)), 'k--')
 plt.plot(x, -dx * np.cumsum(x * occupations.marzari_vanderbilt.delta(x)), 'k--')
 plt.plot(x, -dx * np.cumsum(x * occupations.methfessel_paxton.delta(x)), 'k--')
-plt.plot(x, -dx * np.cumsum(x * occupations.double_fermi_dirac.delta(x, mu1, mu2)), 'k--')
+plt.plot(x, -dx * np.cumsum(x * occupations.double_fermi_dirac.delta(x,
+    mu1, mu2)), 'k--')
 
 plt.xlabel(r'$x$')
 plt.ylabel(r'$-\int_{-\infty}^x y \delta(y) dy$')
