6391c8cb40ba567da67549bb76ec49ee66f0e1a2
Jan Berges <jan.berges@uni-bremen.de>
2024-01-09 09:30:36 +0100

    Build project using pyproject.toml file only

--- setup.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-# Copyright (C) 2017-2023 elphmod Developers
-# This program is free software under the terms of the GNU GPLv3 or later.
-
-import setuptools
-
-setuptools.setup()
