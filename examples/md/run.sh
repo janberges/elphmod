d6fa559ef215a31816ecfcfec4ab036d4974e4fc
Jan Berges <jan.berges@uni-bremen.de>
2024-02-21 16:47:30 +0100

    Get i-PI driver from GitHub if not found

--- examples/md/run.sh
+++ examples/md/run.sh
@@ -3,6 +3,11 @@
 # Copyright (C) 2017-2024 elphmod Developers
 # This program is free software under the terms of the GNU GPLv3 or later.
 
-cp $IPI_ROOT/drivers/py/driver.py .
+if test -n "$IPI_ROOT"
+then
+    cp $IPI_ROOT/drivers/py/driver.py .
+else
+    wget https://raw.githubusercontent.com/i-pi/i-pi/master/drivers/py/driver.py
+fi
 
 python3 driver.py --unix
