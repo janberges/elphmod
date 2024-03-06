52b32f30116b066a4fd45f027c506ba11533face
Jan Berges <jan.berges@uni-bremen.de>
2024-03-06 14:36:23 +0100

    Download i-PI driver only if absent

--- examples/md/run.sh
+++ examples/md/run.sh
@@ -3,11 +3,14 @@
 # Copyright (C) 2017-2024 elphmod Developers
 # This program is free software under the terms of the GNU GPLv3 or later.
 
-if test -n "$IPI_ROOT"
+if test ! -e driver.py
 then
-    cp $IPI_ROOT/drivers/py/driver.py .
-else
-    wget https://raw.githubusercontent.com/i-pi/i-pi/master/drivers/py/driver.py
+    if test -n "$IPI_ROOT"
+    then
+        cp "$IPI_ROOT/drivers/py/driver.py" .
+    else
+        wget https://raw.githubusercontent.com/i-pi/i-pi/master/drivers/py/driver.py
+    fi
 fi
 
 python3 driver.py --unix
