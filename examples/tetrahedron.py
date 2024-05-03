39d251a8d88267098c222b6111f7f60185cabf5e
Jan Berges <jan.berges@uni-bremen.de>
2024-05-03 21:19:22 +0200

    Avoid star import

--- examples/tetrahedron.py
+++ examples/tetrahedron.py
@@ -3,16 +3,16 @@
 # Copyright (C) 2017-2024 elphmod Developers
 # This program is free software under the terms of the GNU GPLv3 or later.
 
-from sympy import *
+import sympy
 
 # We want to calculate:
 #
 # 1/V integral d^2k delta[e1(k)] delta[e2(k)] f(k)
 
 def simpler(x):
-    return factor(expand(x))
+    return sympy.factor(sympy.expand(x))
 
-A, B, C, a, b, c, X, T, t, l = symbols('A B C a b c X T t l')
+A, B, C, a, b, c, X, T, t, l = sympy.symbols('A B C a b c X T t l')
 
 #  X \
 #     \   C______________A'
@@ -33,7 +33,7 @@ k1 = (X - a) / (b - a) * t
 K2 = (X - A) / (C - A) * (1 - T)
 k2 = (X - a) / (c - a) * (1 - t)
 
-solution = solve({K1 - k1, K2 - k2}, {T, t})
+solution = sympy.solve({K1 - k1, K2 - k2}, {T, t})
 
 for key in solution:
     solution[key] = simpler(solution[key])
@@ -55,12 +55,12 @@ K3 = simpler(1 - K1 - K2)
 #
 # with dk1,2/dk = grad(e1,2)
 
-V = l ** 2 * sqrt(3) / 2 # BZ area
+V = l ** 2 * sympy.sqrt(3) / 2 # BZ area
 
 grad1x = (B - A) / l
-grad1y = (C - (A + B) / 2) / (sqrt(3) / 2 * l)
+grad1y = (C - (A + B) / 2) / (sympy.sqrt(3) / 2 * l)
 grad2x = (b - a) / l
-grad2y = (c - (a + b) / 2) / (sqrt(3) / 2 * l)
+grad2y = (c - (a + b) / 2) / (sympy.sqrt(3) / 2 * l)
 
 det = simpler(grad1x * grad2y - grad1y * grad2x)
 
