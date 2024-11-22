Fix sign of model electron-phonon coupling
The previously implemented coupling formulas were derived using the convention
t(R - R') = <R|H|R'> or t(R) = <R|H|0> for the lattice-vector dependence of the
hopping matrix elements. However, elphmod uses the convention t(R) = <0|H|R>.
Consequently, the hopping was not decreasing with increasing bond length.

elphmod/models/chain.py
@@ -55,7 +55,7 @@ def coupling(q1=0, q2=0, q3=0, k1=0, k2=0, k3=0, **ignore):
 
     g = np.zeros((3, 1, 1), dtype=complex)
 
-    g[0, 0, 0] = 1j * g0 * (np.sin(k1) - np.sin(k1 + q1))
+    g[0, 0, 0] = 1j * g0 * (np.sin(k1 + q1) - np.sin(k1))
 
     return g
 

elphmod/models/tas2.py
@@ -302,12 +302,12 @@ def coupling(q1=0, q2=0, q3=0, k1=0, k2=0, k3=0, **ignore):
     K2 = k2 + q2
 
     return (
-        + dt[0] * (np.exp(1j * k1) - np.exp(1j * K1))
-        + dt[3] * (np.exp(-1j * k1) - np.exp(-1j * K1))
-        + dt[1] * (np.exp(1j * (k1 + k2)) - np.exp(1j * (K1 + K2)))
-        + dt[4] * (np.exp(-1j * (k1 + k2)) - np.exp(-1j * (K1 + K2)))
-        + dt[2] * (np.exp(1j * k2) - np.exp(1j * K2))
-        + dt[5] * (np.exp(-1j * k2) - np.exp(-1j * K2))
+        + dt[0] * (np.exp(1j * K1) - np.exp(1j * k1))
+        + dt[3] * (np.exp(-1j * K1) - np.exp(-1j * k1))
+        + dt[1] * (np.exp(1j * (K1 + K2)) - np.exp(1j * (k1 + k2)))
+        + dt[4] * (np.exp(-1j * (K1 + K2)) - np.exp(-1j * (k1 + k2)))
+        + dt[2] * (np.exp(1j * K2) - np.exp(1j * k2))
+        + dt[5] * (np.exp(-1j * K2) - np.exp(-1j * k2))
         ) / sqrtM
 
 def create(prefix=None, rydberg=False, divide_mass=True):