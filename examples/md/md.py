28914e4d6462e34746e3a8c25ac324ffeee6c3ed
Jan Berges <jan.berges@uni-bremen.de>
2025-05-23 13:20:26 +0200

    Increase chances of distorted relaxation result

--- examples/md/md.py
+++ examples/md/md.py
@@ -20,7 +20,7 @@ driver = elphmod.md.Driver(elph, nk=(12, 12), nq=(2, 2), supercell=(9, 9),
 driver.kT = 0.005
 driver.f = elphmod.occupations.fermi_dirac
 
-driver.random_displacements()
+driver.random_displacements(amplitude=0.05)
 
 driver.to_xyz('init.xyz')
 
