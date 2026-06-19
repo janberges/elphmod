3b57bb5575f14a262cd6e23f72c6f06fc9ee156f
Jan Berges <jan.berges@uni-bremen.de>
2026-06-19 09:10:42 +0200

    Test "k2r" as part of the Wannier90 example

--- examples/wannier/wannier.py
+++ examples/wannier/wannier.py
@@ -23,14 +23,25 @@ for seedname, ref, res in ('ws_yes', 'm', 'k--'), ('ws_no', 'g', 'k:'):
 
     if comm.rank == 0:
         for n in range(el.size):
-            plt.plot(X, E[:, n], ref,
+            plt.plot(X, E[:, n], ref, linewidth=2,
                 label=None if n else 'directly from W90 (%s)' % seedname)
 
         for n in range(el.size):
-            plt.plot(x, e[:, n], res,
+            plt.plot(x, e[:, n], res, linewidth=2,
                 label=None if n else 'via elphmod (%s)' % seedname)
 
+k_mesh = elphmod.bravais.mesh(*pwi['k_points'][:3])
+H_mesh = elphmod.dispersion.sample(el.H, k_mesh)
+
+elphmod.el.k2r(el, H_mesh)
+
+e = elphmod.dispersion.dispersion(el.H, k)
+
 if comm.rank == 0:
+    for n in range(el.size):
+        plt.plot(x, e[:, n], 'y-', linewidth=1,
+            label=None if n else 'via elphmod (k2r)')
+
     plt.ylabel('Electron energy (eV)')
     plt.xlabel('Wave vector')
     plt.xticks(x[corners], path)
