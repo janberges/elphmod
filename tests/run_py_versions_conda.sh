e62eac1d9fe6ce9cf05c4149bfb9cf3aa66aee9e
Jan Berges <jan.berges@uni-bremen.de>
2026-07-27 11:10:01 +0200

    Run tests using existing conda environments

--- tests/run_py_versions_conda.sh
+++ tests/run_py_versions_conda.sh
@@ -6,19 +6,30 @@
 env=/dev/shm/env
 log=run_py_versions_conda.log
 
+#unset env # use existing environments py3.5, py3.6, etc.
+
 echo "Tests for different Python versions" > $log
 
 for minor in `seq 5 14`
 do
-    conda create -y -p $env python=3.$minor
-    conda activate $env
+    if test $env
+    then
+        conda create -y -p $env python=3.$minor
+        conda activate $env
 
-    conda install -y numpy scipy
-    conda develop ..
+        conda install -y numpy scipy
+        conda develop ..
+    else
+        conda activate py3.$minor
+    fi
 
     echo "Tests for Python 3.$minor" | tee -a $log
     python3 -m unittest -vfc 2>&1 | tee -a $log
 
     conda deactivate
-    conda remove -y -p $env --all
+
+    if test $env
+    then
+        conda remove -y -p $env --all
+    fi
 done
